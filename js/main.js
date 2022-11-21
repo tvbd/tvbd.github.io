5a66388c367504be499bed66c108cae5b4b9be8f
tvbd <45828705+tvbd@users.noreply.github.com>
2022-11-22 00:32:11 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ player.playlist([
 	{ name: 'GTV2', sources: [ {src: 'http://30.30.30.30/live/skyfeed2001/index.m3u8', type: 'application/x-mpegURL'}, ] },
         { name: 'GTV3', sources: [ {src: 'http://30.30.30.30/live/skyfeed1006/index.m3u8', type: 'application/x-mpegURL'}, ] },	
         { name: 'GTV4', sources: [ {src: 'http://30.30.30.30/live/skyfeed2001/index.m3u8', type: 'application/x-mpegURL'},] },
-	{name: 'T-Sports3', sources: [ {src: 'http://30.30.30.30/live/skyfeed2002/index.m3u8', type: 'application/x-mpegURL'},] },
+	{ name: 'T-Sports3', sources: [ {src: 'http://30.30.30.30/live/skyfeed2002/index.m3u8', type: 'application/x-mpegURL'},] },
+        { name: 'T-Sports4', sources: [ {src: 'http://172.16.128.2:8081/live/sky1010/playlist.m3u8', type: 'application/x-mpegURL'},] },
+        { name: 'T-Sports5', sources: [ {src: 'http://172.16.128.2:8081/live/sky2002/playlist.m3u8', type: 'application/x-mpegURL'},] },
+	{ name: 'GTV5', sources: [ {src: 'http://172.16.128.2:8081/live/sky1011/playlist.m3u8', type: 'application/x-mpegURL'},] },
+	{ name: 'GTV6', sources: [ {src: 'http://172.16.128.2:8081/live/sky2001/playlist.m3u8', type: 'application/x-mpegURL'},] },
 	   ]);
 
 // Initialize the playlist-ui plugin with no option (i.e. the defaults).
