c5ff1aedbfd23353c426108f22a1ee37ba97aa9b
tvbd <45828705+tvbd@users.noreply.github.com>
2023-09-14 19:14:30 +0600

    Update main.js

--- playlist/cup/js/main.js
+++ playlist/cup/js/main.js
@@ -6,7 +6,7 @@ player.playlist([
         { name: 'T-Sports2', sources: [ {src: 'http://103.118.76.146:1935/dhakacdn/tsports.stream/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
 	{ name: 'GTV', sources: [ {src: 'http://103.118.76.146:1935/dhakacdn/gtv.stream/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
         { name: 'Starsports1 tamil', sources: [ {src: 'http://20.2.72.221/asiacup/index.m3u8', type: 'application/x-mpegURL'}, ] },		
-        { name: 'T-Sports4', sources: [ {src: 'http://172.16.128.2:8081/live/sky1010/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
+        { name: 'T-Sports3', sources: [ {src: 'http://11plus.live/live/tsports/index.m3u8', type: 'application/x-mpegURL'}, ] },
         { name: 'T-Sports5', sources: [ {src: 'http://172.16.128.2:8081/live/sky2002/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
 	{ name: 'GTV4', sources: [ {src: 'http://172.16.128.2:8081/live/sky1011/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
 	{ name: 'GTV5', sources: [ {src: 'http://172.16.128.2:8081/live/sky2001/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
