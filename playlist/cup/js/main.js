a16278ca7f68110d184db4a0c6008b8116e8295c
tvbd <45828705+tvbd@users.noreply.github.com>
2023-09-14 18:57:02 +0600

    Update main.js

--- playlist/cup/js/main.js
+++ playlist/cup/js/main.js
@@ -2,12 +2,10 @@ $(document).ready(function () {
     var player = videojs('video');
 player.playlist([
         { name: 'Star sports 1', autoplay: true, sources: [ {src: 'http://172.17.50.112/live/antbd1001/index.m3u8', type: 'application/x-mpegURL'},] },
-        { name: 'T-Sports 1', sources: [  {src: 'http://172.17.50.112/live/antbd1002/index.m3u8', type: 'application/x-mpegURL'}, ] },
-        { name: 'GTV1', sources: [ {src: 'http://172.17.50.112/live/antbd1004/index.m3u8', type: 'application/x-mpegURL'}, ] },
-	{ name: 'T-Sports2', sources: [ {src: 'http://30.30.30.30/live/skyfeed1005/index.m3u8', type: 'application/x-mpegURL'}, ] },
-	{ name: 'T-Sports3', sources: [ {src: 'http://30.30.30.30/live/skyfeed2002/index.m3u8', type: 'application/x-mpegURL'},] },
-	{ name: 'GTV2', sources: [ {src: 'http://30.30.30.30/live/skyfeed2001/index.m3u8', type: 'application/x-mpegURL'}, ] },
-        { name: 'GTV3', sources: [ {src: 'http://30.30.30.30/live/skyfeed1006/index.m3u8', type: 'application/x-mpegURL'}, ] },		
+        { name: 'T-Sports1', sources: [  {src: 'http://172.17.50.112/live/antbd1002/index.m3u8', type: 'application/x-mpegURL'}, ] },
+        { name: 'T-Sports2', sources: [ {src: 'http://103.118.76.146:1935/dhakacdn/tsports.stream/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
+	{ name: 'GTV', sources: [ {src: 'http://103.118.76.146:1935/dhakacdn/gtv.stream/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
+        { name: 'Starsports1 tamil', sources: [ {src: 'http://20.2.72.221/asiacup/index.m3u8', type: 'application/x-mpegURL'}, ] },		
         { name: 'T-Sports4', sources: [ {src: 'http://172.16.128.2:8081/live/sky1010/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
         { name: 'T-Sports5', sources: [ {src: 'http://172.16.128.2:8081/live/sky2002/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
 	{ name: 'GTV4', sources: [ {src: 'http://172.16.128.2:8081/live/sky1011/playlist.m3u8', type: 'application/x-mpegURL'}, ] },
