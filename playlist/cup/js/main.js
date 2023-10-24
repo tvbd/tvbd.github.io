f7c42d3ef0a1a1198508cb9d47dd111195e133d6
TVBD <45828705+tvbd@users.noreply.github.com>
2023-10-24 17:55:36 +0600

    Update main.js

--- playlist/cup/js/main.js
+++ playlist/cup/js/main.js
@@ -2,15 +2,14 @@ $(document).ready(function () {
     var player = videojs('video');
 player.playlist([
         
-{ name: 'Ptv', autoplay: true, sources: [ {src: 'http://11plus.live/live/ptv/index.m3u8', type: 'application/x-mpegURL'},]},
-{ name: 'TSports', sources: [ {src: 'http://11plus.live/live/tsports/index.m3u8', type: 'application/x-mpegURL'}, ] },
-{ name: 'Star Sports 1', sources: [ {src: 'http://11plus.live/live/stersports1hd/index.m3u8', type: 'application/x-mpegURL'}, ] },
-{ name: 'Star sports 1', sources: [  {src: 'http://172.17.50.112/live/fifa2022/index.m3u8', type: 'application/x-mpegURL'}, ] },
+{ name: 'Ptv',           autoplay: true, sources: [ {src: 'http://11plus.live/live/ptv/index.m3u8', type: 'application/x-mpegURL'},]},
+{ name: 'TSports',       autoplay: true, sources: [ {src: 'http://11plus.live/live/tsports/index.m3u8', type: 'application/x-mpegURL'}, ] },
+{ name: 'Star Sports 1', autoplay: true, sources: [ {src: 'http://11plus.live/live/stersports1hd/index.m3u8', type: 'application/x-mpegURL'}, ] },
+{ name: 'Star sports 1', autoplay: true, sources: [  {src: 'http://172.17.50.112/live/fifa2022/index.m3u8', type: 'application/x-mpegURL'}, ] },
 { name: 'Star sports 1', autoplay: true, sources: [ {src: 'http://172.17.50.112/live/antbd1001/index.m3u8', type: 'application/x-mpegURL'},] },
-{ name: 'TSports', sources: [  {src: 'http://172.17.50.112/live/antbd1002/index.m3u8', type: 'application/x-mpegURL'}, ] },
-{ name: 'Starsports1 tamil', sources: [ {src: 'http://20.2.72.221/asiacup/index.m3u8', type: 'application/x-mpegURL'}, ] },		
-{ name: 'T-Sports', sources: [ {src: 'https://ott.sonyplex.com:444/play/y4ypVArrswAkLijdKS_tnhhIvk9niht3vh2W4ClrIzo/m3u8', type: 'application/x-mpegURL'}, ] },
-{ name: 'GTV', sources: [ {src: 'https://ott.sonyplex.com:444/play/V7bMhseWvzq2L18X8hGbefmhw9o1yNtqn1S2CY6Yyi8/m3u8', type: 'application/x-mpegURL'}, ] },
+{ name: 'TSports',       autoplay: true, sources: [  {src: 'http://172.17.50.112/live/antbd1002/index.m3u8', type: 'application/x-mpegURL'}, ] },	
+{ name: 'T-Sports',      autoplay: true, sources: [ {src: 'https://ott.sonyplex.com:444/play/y4ypVArrswAkLijdKS_tnhhIvk9niht3vh2W4ClrIzo/m3u8', type: 'application/x-mpegURL'}, ] },
+{ name: 'GTV',           autoplay: true, sources: [ {src: 'https://ott.sonyplex.com:444/play/V7bMhseWvzq2L18X8hGbefmhw9o1yNtqn1S2CY6Yyi8/m3u8', type: 'application/x-mpegURL'}, ] },
 
 ]);
 
