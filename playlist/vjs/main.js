63aa06e19812c1e2b34fe1b71e0e8f77c23c5556
TVBD <45828705+tvbd@users.noreply.github.com>
2023-10-24 15:55:58 +0600

    Update main.js

--- playlist/vjs/main.js
+++ playlist/vjs/main.js
@@ -40,9 +40,8 @@ $(document).ready(function () {
             sources: [
                 {src: 'http://172.17.50.112/live/antbd1002/index.m3u8', type: 'application/x-mpegURL'},
             ]
-        }
-            ]
-        }]);
+                }
+		]);
 
 // Initialize the playlist-ui plugin with no option (i.e. the defaults).
     player.playlistUi();
