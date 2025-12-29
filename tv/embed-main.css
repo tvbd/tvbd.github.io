dffb1933c2f1ea0c1d366ff1eac0d95939ec530b
TVBD <45828705+tvbd@users.noreply.github.com>
2025-12-29 19:29:41 +0600

    Add files via upload

---
+++ tv/embed-main.css
@@ -0,0 +1,101 @@
+* {
+    margin: 0;
+    padding: 0;
+}
+html, body{
+    width: 100%;
+    height: 100%;
+}
+.wrapper{
+    position: relative;
+}
+.video-js {
+    width: auto !important;
+    height: auto !important;
+    padding-top: 56.25% !important;
+    min-height: 250px;
+    max-width: 100%;
+}
+
+.vjs-fullscreen {
+    padding-top: 0px !important;
+}
+.ads{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    display: block;
+    z-index: 9999;
+    background: #000000;
+}
+.ads .block {
+    display: inline-block;
+    position: relative;
+    overflow: hidden;
+    top: 50%;
+    min-width: 300px;
+    /* min-height: 220px; */
+    /* max-height: 80%; */
+    -webkit-transform: translateY(-50%);
+    -moz-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    -o-transform: translateY(-50%);
+    transform: translateY(-50%);
+    /*  padding-bottom: 20px;  */
+}
+
+.btn-close-ads{
+    font-family: Arial;
+    font-size: 12px;
+    cursor: pointer;
+    z-index: 99999;
+    text-align: center;
+    color: #FFFFFF;
+    background: #5B5B5B;
+    cursor: pointer;
+    height: 20px;
+    line-height: 20px;
+    text-align: center;
+    text-decoration: none;
+    width: 300px;
+    display: block;
+}
+.alert-type {
+    position: absolute;
+    top: 10px;
+    width: 90%;
+    left: 0;
+    right: 0;
+    margin: auto;
+    font-family: Arial;
+    text-align: center;
+    color: #FFFFFF;
+    background: #000000;
+    padding: 5px;
+    border-radius: 5px;
+    z-index: 1;
+}
+.adsdisplay {
+    overflow: hidden;
+    width: 300px;
+    height: 250px;
+}
+#content_video_ima-ad-container { z-index: 0 !important;}
+.vjs-big-play-button {z-index: 9999999;}
+#rcjsload_fe24df{
+    padding-top: 35px;
+}
+@media only screen and (max-width: 500px) {
+    .adsdisplay,.adsdisplay iframe {
+        height: 210px !important;
+        margin: 0 auto;
+        overflow: hidden;
+    }
+    #rcjsload_fe24df{
+        padding-top: 0px;
+    }
+
+}
