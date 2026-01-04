cf5781f3e7b71d0e7c71c268f5c164bdac90c7a4
TVBD <45828705+tvbd@users.noreply.github.com>
2026-01-05 01:06:09 +0600

    Update app.js

--- tv/crichd/app.js
+++ tv/crichd/app.js
@@ -1,3 +1,3 @@
 function play(id){
-  location.href = "player.html/?fire=https://crichd1.xfireflixbd.workers.dev/?v=" + encodeURIComponent(id);
+  location.href = "player.html?fire=https://crichd1.xfireflixbd.workers.dev?v=" + encodeURIComponent(id);
 }
