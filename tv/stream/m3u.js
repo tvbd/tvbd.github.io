74c92176e4a2e9ee8f9de2f6476c7c566b90047f
TVBD <45828705+tvbd@users.noreply.github.com>
2026-02-11 12:58:53 +0600

    Add files via upload

---
+++ tv/stream/m3u.js
@@ -0,0 +1,14 @@
+const defaultM3U = `#EXTM3U
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", Prime
+https://cricketstan.github.io/Allrounder-/prame
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", Prime Hindi    
+https://all-in-one-channel.vercel.app/PRIME/DILLZY/
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", HOTSTAR
+https://allrounderlive.pages.dev/dilz?id=65656576
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed",Willow
+https://allrounderlive.pages.dev/willow
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed",Sky Uk
+https://allrounderlive.pages.dev/dilz?id=dillzy645
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", Prime HD
+https://skcricweb.pages.dev/ipel?id=t20wc
+`;
