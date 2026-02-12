ab26e68c93bb17cc48a754c9e2ba526c0fe2cad3
TVBD <45828705+tvbd@users.noreply.github.com>
2026-02-12 21:25:38 +0600

    Update m3u.js

--- tv/stream/m3u.js
+++ tv/stream/m3u.js
@@ -1,14 +1,17 @@
-const defaultM3U = `#EXTM3U
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", Prime
+const defaultM3U = `
+#EXTM3U
+#EXTINF:-1 tvg-logo="https://i.imgur.com/ZUuvx2S.png" group-title="M3U", Geo Super
+https://ml-pull-dvc-myco.io:2096/GEO_SUPER/tracks-v1a1/mono.ts.m3u8
+#EXTINF:-1 tvg-logo="https://i.imgur.com/ZUuvx2S.png" group-title="Embed", Prime
 https://cricketstan.github.io/Allrounder-/prame
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", Prime Hindi    
+#EXTINF:-1 tvg-logo="https://i.imgur.com/ZUuvx2S.png" group-title="Embed", Prime Hindi    
 https://all-in-one-channel.vercel.app/PRIME/DILLZY/
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", HOTSTAR
+#EXTINF:-1 tvg-logo="https://i.imgur.com/ZUuvx2S.png" group-title="Embed", HOTSTAR
 https://allrounderlive.pages.dev/dilz?id=65656576
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed",Willow
+#EXTINF:-1 tvg-logo="https://i.imgur.com/ZUuvx2S.png" group-title="Embed",Willow
 https://allrounderlive.pages.dev/willow
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed",Sky Uk
+#EXTINF:-1 tvg-logo="https://i.imgur.com/ZUuvx2S.png" group-title="Embed",Sky Uk
 https://allrounderlive.pages.dev/dilz?id=dillzy645
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f8/2026_ICC_Men%27s_T20_World_Cup_logo.png" group-title="Embed", Prime HD
+#EXTINF:-1 tvg-logo="https://i.imgur.com/ZUuvx2S.png" group-title="Embed", Prime HD
 https://skcricweb.pages.dev/ipel?id=t20wc
 `;
