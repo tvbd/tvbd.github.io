1b62332f4773ed62fa72ce2bfda0f276cb3a6773
TVBD <45828705+tvbd@users.noreply.github.com>
2026-02-12 22:48:03 +0600

    Add files via upload

---
+++ tv/extender/m3u.js
@@ -0,0 +1,12 @@
+const streams = {
+            1: { hls: "https://ml-pull-dvc-myco.io:2096/GEO_SUPER/tracks-v1a1/mono.ts.m3u8" },
+            2: { mpd: "https://a120live-pv-ta-amazon.akamaized.net/sin-nitro/live/clients/dash/enc/cjfgjgm8nr/out/v1/4b134e64cd5e4be38f9d00bb96fc4a0a/cenc.mpd", keyId: "e2135bf415cfea9ea3e6b26a6fce8fc3", key: "2671da359a0a860668baca2e34f243f6" },
+            3: { mpd: "https://a166aivottlinear-a.akamaihd.net/OTTB/bom-nitro/live/dash/enc/igjuwt107o/out/v1/489e489428ce4992b9bd784d6b948ab3/cenc.mpd", keyId: "b07e9785466b831915621dc51ed50d3c", key: "af30a9d18114259f21a3cc9e34ff649d" },
+            4: { mpd: "https://a201aivottlinear-a.akamaihd.net/OTTB/lhr-nitro/live/dash/enc/ksczi4sz0e/out/v1/a3f76c40f2c74804876697c4c284780f/cenc.mpd", keyId: "02b6e65ff2e3c3cf55a52426e03161dc", key: "8cd7fa1463f0e7624725e89389d181e6" },
+            5: { mpd: "https://otte.live.fly.ww.aiv-cdn.net/sin-nitro/live/clients/dash/enc/glyngcujgc/out/v1/02c5d30ca7f744bc8794243145a8abbc/cenc.mpd", keyId: "9b46f722b6b64d4830629d4341b837a2", key: "c6c925d12a2f5e982ed83e508407b25c" },
+            6: { hls: "https://amitomar.hoichoi.pro/bdixbd.net/index.fmp4.m3u8" },
+            7: { hls: "https://live-pull.aisports.mobi/moviebox/device01/playlist.m3u8" },
+            8: { hls: "https://live-pull.aisports.mobi/moviebox/device02/playlist.m3u8" },
+            9: { hls: "https://live-pull.aisports.mobi/moviebox/football001/playlist.m3u8" },
+            10: { hls: "https://premierleagpl23.akamaized.net/hls/live/2107108/Tapmad-25852-Seriea-1.2/master.m3u8" },
+        };
