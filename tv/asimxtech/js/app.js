be8958e067e1bc3957925fe780222e2649457164
TVBD <45828705+tvbd@users.noreply.github.com>
2026-02-20 20:32:06 +0600

    Update app.js

--- tv/asimxtech/js/app.js
+++ tv/asimxtech/js/app.js
@@ -435,7 +435,7 @@
       headers["X-Client-Country"] = countryHint;
     }
 
-    const response = await fetch("/tv.json", {
+    const response = await fetch("//raw.githubusercontent.com/tvbd/tvbd.github.io/master/tv/asimxtech/tv.json", {
       cache: "no-store",
       headers
     });
